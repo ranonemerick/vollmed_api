74c1c3a6770605aa4fadf82f976c8eef51ee5e68
Ranon Emerick <ranonemerick@gmail.com>
2023-03-13 10:39:49 -0300

    med-voll-api-med-pac

---
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -0,0 +1,12 @@
+package med.voll.api.endereco;
+
+public record DadosEndereco(
+        String logradouro,
+        String numero,
+        String complemento,
+        String bairro,
+        String cidade,
+        String uf,
+        String cep) {
+
+}
---
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,36 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Entity
+@Table(name = "medicos")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+    @Embedded
+    private Endereco endereco;
+
+    public Medico(DadosCadastroMedico dados) {
+        this.nome = dados.nome();
+        this.crm = dados.crm();
+        this.email = dados.email();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
+}
